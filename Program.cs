ea3c7788501371bef9da4f3306ff31bec3a0704b
sharon088 <49710947+sharon088@users.noreply.github.com>
2024-11-06 16:24:37 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
 Console.WriteLine("Hello, World!");
+Execute().Wait();
+
+static async Task Execute()
+{
+    var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+    var client = new SendGridClient(apiKey);
+    var from = new EmailAddress("sharon088@gmail.com", "sharon User");
+    var subject = "Sending with SendGrid is Fun";
+    var to = new EmailAddress("rotem23592@gmail.com", "Example User");
+    var plainTextContent = "and easy to do anywhere, even with C#";
+    var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+    var response = await client.SendEmailAsync(msg);
+}
